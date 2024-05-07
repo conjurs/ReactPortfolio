05f179192b5981834d201a97e771227cd1e917b6
Axel / Conjurs <113966806+conjurs@users.noreply.github.com>
2024-05-07 13:39:20 +0300

    Upld

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+import Kali from '../assets/projects/kali.png'
+import Weather from '../assets/projects/weather.png'
+import Twitter from '../assets/projects/twitter.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React TS Kali Terminal Portfolio",
+        image:Kali,
+        github:"https://github.com/conjurs/kaliportfolio",
+    },
+    {
+        id:2,
+        name:"React JS Weather Information",
+        image:Weather,
+        github:"https://github.com/conjurs/weather-checker",
+    },
+    {
+        id:3,
+        name:"React TS Twitter Remake",
+        image:Twitter,
+        github:"https://github.com/conjurs/Tewttr",
+    },
+]
